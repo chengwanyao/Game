65eadd316c99eeff50d1d74107136593aa480a03
cwy <1041031171@qq.com>
2017-01-09 15:20:10 +0800

    Finish Homework Final

---
+++ src/Package.ts
@@ -0,0 +1,16 @@
+class Package{
+    private backpack : Equipment[] = [];
+    private currentSize = 0;
+
+    public InPackage(equipemt : Equipment){
+        this.backpack.push(equipemt);
+        equipemt.numInPackage = this.currentSize;
+        this.currentSize++;
+    }
+
+
+    public OutPackage(equipemt : Equipment){
+        this.backpack.splice(equipemt.numInPackage);
+        this.currentSize--;
+    }
+}
